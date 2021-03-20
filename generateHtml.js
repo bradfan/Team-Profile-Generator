505e5bdc6467a8da605225f7803647560f290cd9
jcerb <brt.jst@gmail.com>
2021-03-19 17:16:46 -0700

    add dependency begin code

--- generateHtml.js
+++ generateHtml.js
@@ -10,4 +10,4 @@
 
 
 
-module.exports = generateHtml;
+// module.exports = generateHtml.js;
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 console.log("index.js says hello");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require("./generateHtml.js");
+// const generateHtml = require("./generateHtml.js");
+
+inquirer.prompt([
+
+ ])
+// .then(function(answers){
+//     const htmlString = ``;
+//     // paste HTML info here between the tick marks above.
+//     console.log("data:", answers);
+//     fs.writeFile("index.html", htmlString, ()=>{
+//         console.log("success")
+//     })
+// })
